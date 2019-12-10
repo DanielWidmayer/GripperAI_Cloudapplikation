302c5765cb858784078fb8129b22b38ec151095c
David Klatte <david.klatte@festo.com>
2019-12-10 15:30:25 +0100

    Erster Commit, Home, Gripper, erster Webcam Versuch der nicht funktioniert

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="templates")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/gripper")
+def gripper():
+    return render_template("gripper.html")
+
+@app.route("/grippingpoints")
+def grippingpoints():
+    return render_template("grippingpoints.html")
+
+@app.route("/live")
+def live():
+    return "This service is still under construction. Stay tuned!"
+if __name__ == "__main__":
+    app.run(debug=True, host = "0.0.0.0", port = 7070 )
