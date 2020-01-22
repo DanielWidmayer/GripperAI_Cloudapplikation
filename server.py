d72c612aa06d258bd70f258f6dc0e773c612d89e
Daniel <david.klatte@festo.com>
2020-01-22 11:37:27 +0100

    chang. call

--- server.py
+++ server.py
@@ -35,5 +35,5 @@ def application():
 def live():
     return "This service is still under construction. Stay tuned!"
 
-if __name__ == "__main__":
-    app.run(  debug=False, host = "localhost", port = 5000 ) 
+#if __name__ == "__main__":
+#    app.run(  debug=False, host = "localhost", port = 5000 ) 
